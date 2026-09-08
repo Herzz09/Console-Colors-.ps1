1eb678ee7f5a9d4bc027dde1b7454d4291898b2d
Herzz09 <brunobatistaherzogerath@gmail.com>
2026-09-08 19:12:58 -0300

    Add ISE execution guard for Console-Colors script
    Console-Colors.ps1 previously assumed it would always be run inside PowerShell ISE, since -B and -F color output is only meant to be launched and inspected from that environment.
    
    This commit adds a guard clause that checks the automatic $psISE
    variable at startup. If the script is executed directly from the
    console instead of the ISE, it now:
    
    - Prints an error message to the console explaining that the script
      cannot be run from the console
    - Automatically opens the script in PowerShell ISE via Start-Process
    - Exits the console session cleanly, avoiding partial or unintended
      execution outside the ISE
    
    Also fixed a typo in the header comment block ("Dame" -> "Name").
    
    No changes were made to the core color-listing logic.

--- Console-Colors.ps1
+++ Console-Colors.ps1
@@ -1,22 +1,37 @@
-<#
-Header:
-dev Herzz09
-date 06/04/2026
-name Console-Colors.ps1
+# Header:
+# Dev Herzz09
+# Date 06/04/2026
+# Name Console-Colors.ps1
 
+#######################################################################
+<#
 `t = Tab
 -B = BackgroundColor
 -F = ForegroundColor
 #>
 
+#######################################################################
 cls
+ 
+#######################################################################
+if (-not $psISE) {
+    Write-Host "ERROR: This script cannot be run from the console. Opening in PowerShell ISE..." -ForegroundColor Red
+    Start-Process powershell_ise.exe -ArgumentList "`"$PSCommandPath`""
+    exit
+}
+#######################################################################
+ 
 write-host "
-  Console Colors:
+Console Colors Powershell:
 "
+ 
+#######################################################################
 foreach ($color in [enum]::GetValues([System.ConsoleColor])) {
     Write-Host "`t`t" -B $color -NoNewline
     Write-Host " => $color" -F $color
 }
-
-write-host ''
-Read-Host "Press Enter to continue..."
+ 
+#######################################################################
+Write-Host ""
+pause
+ 
